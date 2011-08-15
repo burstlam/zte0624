008f9424f59bad568318472adbedcc72acf2aa50
burst lam <burstlam@gmail.com>
2011-08-16 02:51:39 +0800

    Enable memcopy function

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -43,9 +43,9 @@
 
 
 /*
- * max 16 partitions per card
+ * max 32 partitions per card
  */
-#define MMC_SHIFT	4
+#define MMC_SHIFT	5
 #define MMC_NUM_MINORS	(256 >> MMC_SHIFT)
 
 static DECLARE_BITMAP(dev_use, MMC_NUM_MINORS);
@@ -122,11 +122,7 @@ static void mmc_blk_put(struct mmc_blk_data *md)
 	mutex_lock(&open_lock);
 	md->usage--;
 	if (md->usage == 0) {
-		int devmaj = MAJOR(disk_devt(md->disk));
-		int devidx = MINOR(disk_devt(md->disk)) >> MMC_SHIFT;
-
-		if (!devmaj)
-			devidx = md->disk->first_minor >> MMC_SHIFT;
+		int devidx = md->disk->first_minor >> MMC_SHIFT;
 
 		blk_cleanup_queue(md->queue.queue);
 
