3012a84069fbae8751174f87bf8fbe1a79a08a02
burst lam <burstlam@gmail.com>
2011-08-29 17:02:49 +0800

    Some Tweaks and fixes

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -639,7 +639,7 @@ static inline struct address_space *page_mapping(struct page *page)
 		mapping = &swapper_space;
 	else
 #endif
-	if (unlikely((unsigned long)mapping & PAGE_MAPPING_ANON))
+	if ((unsigned long)mapping & PAGE_MAPPING_ANON)
 		mapping = NULL;
 	return mapping;
 }
@@ -1170,8 +1170,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	4096	/* kbytes */
-#define VM_MIN_READAHEAD	32	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	512	/* kbytes */
+#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
